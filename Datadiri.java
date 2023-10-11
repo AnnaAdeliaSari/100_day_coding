78a43852b3972d2340969c86c41e6049a38a403e
AnnaAdeliaSari <147259007+AnnaAdeliaSari@users.noreply.github.com>
2023-10-11 19:15:05 +0800

    day3
    membuat sebuah kelas bernama "Datadiri." Dengan variabel-variabel untuk menyimpan informasi pribadi, seperti nama, alamat, usia, dan tinggi, kemudian mencetak informasi tersebut ke layar.

---
+++ Datadiri.java
@@ -0,0 +1,20 @@
+package day3;
+
+public class Datadiri {
+    public static void main(String[] args) {
+        String nama, alamat;
+        int usia;
+        double tinggi;
+        
+        nama ="Anna Adelia Sari";
+        alamat ="Majene";
+        usia = 19;
+        tinggi = 150.5;
+        
+        System.out.println("Nama: " + nama);
+        System.out.println("Alamat: " + alamat);
+        System.out.println("Usia: " + usia);
+        System.out.println("Tinggi: " + tinggi);
+    }
+    
+}
